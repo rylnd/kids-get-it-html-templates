a00d9ee2b90dd3273372f99b4056d23df6ed1fa8
Mic Funk <mic@bendyworks.com>
2014-12-05 19:59:28 -0600

    Quick comment updates

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,5 +1,7 @@
 /* put any javascript in here */
 
 $(document).ready(function() {
+  /* just a quick test to make sure it works. delete for real site */
   $('body').addClass('it-works');
+  /* put any jquery here if necessary */
 });
