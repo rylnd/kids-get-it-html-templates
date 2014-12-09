31650d7ad4657c1af7938eb3b5c005993d928ec6
Ryland Herrick <ryalnd@gmail.com>
2014-12-08 22:25:55 -0600

    Start carousel on page load
    Add HTML markup needed for carousel.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,9 @@
 /* put any javascript in here */
 
 $(document).ready(function() {
-  /* just a quick test to make sure it works. delete for real site */
-  $('body').addClass('it-works');
-  /* put any jquery here if necessary */
+  $('.owl-carousel').owlCarousel({
+    autoPlay: true,
+    singleItem: true,
+    autoResize: true
+  });
 });
